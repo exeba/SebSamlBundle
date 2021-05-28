de5c68165693dca16efa86eecf95c212f9bc0653
Sebastiano Degan <sebastiano@sebastiano.in.ditta>
2021-05-28 11:44:55 +0200

    made controller public and moved most of service defs into yaml file

--- src/DependencyInjection/SebSamlExtension.php
+++ src/DependencyInjection/SebSamlExtension.php
@@ -2,21 +2,13 @@
 
 namespace Seb\SamlBundle\DependencyInjection;
 
-use OneLogin\Saml2\Auth;
-use Seb\AuthenticatorBundle\Security\Guard\Authenticator;
 use Seb\AuthenticatorBundle\Security\Policies\CreateUserIfAuthSucceeds;
-use Seb\AuthenticatorBundle\Security\Policies\RedirectOnBadCredentials;
-use Seb\AuthenticatorBundle\Security\Policies\TargetPathOrHomePageRedirect;
 use Seb\AuthenticatorBundle\Security\Policies\ThrowOnMissingUser;
-use Seb\SamlBundle\Controller\SamlController;
-use Seb\SamlBundle\Security\SamlAttributesStorage;
-use Seb\SamlBundle\Security\SamlAuthenticatedTokenProvider;
-use Seb\SamlBundle\Security\SamlCredentialsChecker;
-use Seb\SamlBundle\Security\SamlCredentialsProvider;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class SebSamlExtension extends Extension
 {
@@ -25,33 +17,14 @@ class SebSamlExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setDefinition('seb_saml.auth', $this->buildOneLoginAuthDef($config));
-        $container->setDefinition('seb_saml.attr_storage', new Definition(SamlAttributesStorage::class));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
 
-        $authGuard = $this->buildAuthGuardDef($config);
+        $container->setParameter('seb_saml.config', $config);
+        $container->setParameter('seb_saml.config.one_login_settings', $config['onelogin_saml_settings']);
+        $container->setParameter('seb_saml.config.failure_path', $config['failure_path']);
 
-        $container->setDefinition('seb_saml.guard', $authGuard);
-
-        $container->setDefinition(SamlController::class,
-            (new Definition(SamlController::class))->setArgument(0, new Reference('seb_saml.auth')));
-    }
-
-    public function buildAuthGuardDef($config)
-    {
-        $authGuard = new Definition(Authenticator::class);
-        $authGuard->setArgument('$credentialsProvider', $this->buildCredentialsProviderDef($config));
-        $authGuard->setArgument('$credentialsChecker', new Definition(SamlCredentialsChecker::class));
-        $authGuard->setArgument('$authenticatedTokenProvider',
-            (new Definition(SamlAuthenticatedTokenProvider::class))
-                ->setArgument('$attributesStorage', new Reference('seb_saml.attr_storage'))
-        );
-        $authGuard->setArgument('$missingUserPolicy', $this->missingUserPolicyDefinition($config));
-        $authGuard->setArgument('$badCredentialsPolicy', $this->badCredentialsPolicyDefinition($config));
-        // TODO: allow ignore target path
-        $authGuard->setArgument('$successfulAuthenticationPolicy',
-            (new Definition(TargetPathOrHomePageRedirect::class))->setAutowired(true));
-
-        return $authGuard;
+        $container->setDefinition('seb_saml.missing_user', $this->missingUserPolicyDefinition($config));
     }
 
     public function missingUserPolicyDefinition(array $config)
@@ -65,33 +38,5 @@ class SebSamlExtension extends Extension
         $missingUser->setAutowired(true);
 
         return $missingUser;
-    }
-
-    public function badCredentialsPolicyDefinition(array $config)
-    {
-        $policy = new Definition(RedirectOnBadCredentials::class);
-        $policy->setAutowired(true);
-        $policy->setArgument(0, $config['failure_path']);
-
-        return $policy;
-    }
-
-    public function buildCredentialsProviderDef($config)
-    {
-        $credentialsProvider = new Definition(SamlCredentialsProvider::class);
-        $credentialsProvider->setAutowired(true);
-        $credentialsProvider->setArgument('$oneLoginAuth', new Reference('seb_saml.auth'));
-        $credentialsProvider->setArgument('$attributesStorage', new Reference('seb_saml.attr_storage'));
-        $credentialsProvider->setArgument('$options', $config);
-
-        return $credentialsProvider;
-    }
-
-    public function buildOneLoginAuthDef($config)
-    {
-        $samlAuth = new Definition(Auth::class);
-        $samlAuth->setArgument(0, $config['onelogin_saml_settings']);
-
-        return $samlAuth;
     }
 }
